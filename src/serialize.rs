6bd638f3763552224c5c87490eb1beb216e74abf
Raul Gherman <raul.gherman@protonmail.com>
2024-12-23 20:37:36 +0200

    0.5.0
    remove btoi

--- src/serialize.rs
+++ src/serialize.rs
@@ -114,5 +114,9 @@ fn is_crlf(
 
 #[inline]
 fn parse_integer(bytes: &[u8]) -> std::result::Result<i64, String> {
-    btoi::btoi::<i64>(bytes).map_err(|e| e.to_string())
+    // btoi::btoi::<i64>(bytes).map_err(|e| e.to_string());
+    std::str::from_utf8(&bytes)
+        .unwrap()
+        .parse::<i64>()
+        .map_err(|e| e.to_string())
 }
